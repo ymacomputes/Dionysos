2ba64fbd1b1282305ec0db61e49a029ec906a675
Amy Collins <amy.collins49@mail.dcu.ie>
2022-07-25 17:46:13 +0100

    added petal counter & guessing feature

--- dice-roll.py
+++ dice-roll.py
@@ -4,7 +4,7 @@ import random
 min = 1
 max = 6
 
-roll = input("Want to roll the dice, y/n?")
+roll = input("Want to roll the dice, y/n? ")
 one = ["-----", "|   |", "| * |", "|   |", "-----"]
 two = ["-----", "|*  |", "|   |", "|  *|", "-----"]
 three = ["-----", "|*  |", "| * |", "|  *|", "-----"]
@@ -12,9 +12,9 @@ four = ["-----", "|* *|", "|   |", "|* *|", "-----"]
 five = ["-----", "|* *|", "| * |", "|* *|", "-----"]
 six = ["-----", "|* *|", "|* *|", "|* *|", "-----"] 
 
-diceRoll = []
-
 while roll == "y":
+    diceRoll = []
+    petalNum = 0
     i = 0
     while i < 6:
         num = random.randint(min, max)
@@ -24,10 +24,12 @@ while roll == "y":
             diceRoll.append(two)
         elif num == 3:
             diceRoll.append(three)
+            petalNum += 2
         elif num == 4:
             diceRoll.append(four)
         elif num == 5:
             diceRoll.append(five)
+            petalNum += 4
         else:
             diceRoll.append(six)
         i += 1
@@ -38,5 +40,14 @@ while roll == "y":
             print(die[row], end="\t")
         print("\r")
         row = row + 1
+
+    petals = input("How many petals are there? ")
+    for a in range(2):
+        if int(petals) == petalNum:
+            print("Congrats! You are correct.")
+            break
+        else:
+            petals = input("Try again. ")
     
-    roll = input("Want to roll the dice?")
+    print("There are " + str(petalNum) + " petals around the rose. ")
+    roll = input("Want to roll again? ")
